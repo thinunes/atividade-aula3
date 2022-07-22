2992413bfe9a8fdc2bdfd27f8a2b0c2c5ccfe66c
thinunes <thiago.tnunes@mercadolivre.com>
2022-07-22 16:32:30 -0300

    atividade express

--- app.js
+++ app.js
@@ -1,26 +1,87 @@
 const express = require('express');
 
+
+let produtos = require("./produtos")
+
 const app = express();
 
-
-
-
-
-app.get('/', (req, res)=>{
-    
-        res.send('Olá mundo ola ola')
-
-    
-       
-   
-    
-
-})
-
+app.use(express.json())
 
 app.get('/produtos', (req,res)=>{
-    res.send('essa é a rota de produtos mais legais')
+    res.status(200).json(produtos)
 })
+
+
+app.post('/produtos' , (req, res)=>{
+
+    const content = req.body
+
+    produtos = [...produtos, content]
+
+    res.status(201).json(produtos)
+})
+
+
+
+app.get('/produtos/:id/:details?', (req, res)=>{
+
+    const id = Number(req.params.id)
+
+    const details = req.params.details || "sem descrição"
+
+    const product = produtos.find((produto)=> produto.id === id)
+
+    res.status(200).json(product)
+})
+
+app.put('/produtos/:id', (req, res)=>{
+    const id = Number(req.params.id)
+    const content = req.body
+
+    const product = produtos.find((produto)=> produto.id === id)
+
+    if(!product){
+        res.status(400).json({"mensage": "produto noa encontrado"})
+    }
+
+
+   const produtoAtt =  produtos.map((produto)=>{
+        if(produto.id === id){
+            return content
+        }
+        return produto
+    })
+
+    
+    produtos = produtoAtt
+res.status(200).json(produtos)
+
+})
+
+
+
+app.delete('/produtos/:id', (req, res)=>{
+    const id = Number(req.params.id)
+
+    const product = produtos.find((produto)=> produto.id === id)
+
+    if(!product){
+        res.status(400).json({"mensage": "produto noa encontrado"})
+    }
+
+    produtos = produtos.filter((produto)=>{
+
+        return produto.id !== id
+
+    })
+
+    res.status(200).json(produtos)
+
+})
+
+
+
+
 
 
 app.listen(3001, ()=>{console.log("Servidor online")})
---
+++ produtos.js
@@ -0,0 +1,15 @@
+let products = [
+    { name: 'Macbook', price: 1300, quantity: 40, colors: ['silver', 'black', 'white'], id: 1 },
+    { name: 'Iphone', price: 1000, quantity: 50, colors: ['silver', 'red', 'white'], id: 2 },
+    { name: 'Pendrive', price: 10, quantity: 10, colors: [], id: 3 },
+    { name: 'Headset', price: 50, quantity: 0, colors: ['black'] },
+    { name: 'Mouse', price: 20, quantity: 5, colors: ['white', 'black', 'blue'], id: 4 },
+    { name: 'Tablet', price: 500, quantity: 20, colors: ['white', 'black'], id: 5 },
+    { name: 'USB adaptor', price: 5, quantity: 0, colors: [], id: 6 },
+    { name: 'Keyboard', price: 30, quantity: 35, colors: ['white'], id: 7 },
+    { name: 'Gamepad', price: 30, quantity: 25, colors: ['black', 'silver'], id: 8 },
+    { name: 'Monitor', price: 200, quantity: 3, colors: [], id: 9 },
+    ]
+
+
+    module.exports = products
